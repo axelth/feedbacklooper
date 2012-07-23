62df9c7b3a738a63b10d0eeaa64bf29860ed807f
AxelT <axel.theorin@gmail.com>
2012-07-23 10:35:55 +0900

    Implemented: get error offset and add a rudimentary error object to an error array

--- master-frontend.js
+++ master-frontend.js
@@ -1,14 +1,39 @@
 //cursor position fetching code inspired by Diego Perini
 //javascript.nwbox.com/cursor_position/
-var Errors = [];
-var currentError;
+var glob = {
+"errorArray": [],
+"currentError":{},
+//modifies currentError
+"createErrorStub": function createErrorStub(errorType) {
+    var submission = document.getElementById("originaltext");
+    var text = submission.value;
+    var selStart = submission.selectionStart;
+    var selEnd = submission.selectionEnd;
+    window.alert([errorType,String(selStart), String(selEnd),
+		  text.substring(selStart,selEnd)].join(' '));
+    this.currentError = {'type':errorType,'start':selStart,
+		    'end':selEnd, 'string':text.substring(selStart,selEnd),
+		   'action':""};
+},
+//modifies errorArray
+"addActionToErrorStub": function(action) {
+    this.currentError.action = action;
+    window.alert("action: " + action + " added to error\n" + this.currentError.toString());
+    this.errorArray.push(this.currentError);
+    this.updateErrorList();
+},
 
-function createError(errorType) {
-var error = {};
-error.pos = getErrorOffsets();
-error.type = errorType
-currentError = error 
-}
-
-function getErrorOffset() {
-start = getSelectionStart(document.studenttext.originaltext)
+//Make sure that errorlist matches errorArray
+//The simplest way is, I guess to build a new table from errorArray and then
+//replace the entire table with that.
+//in that case, I need to sort the errorArray based in index.
+//I also need to check for overlapping errors, which I can't handle yet
+"updateErrorList":function(){
+    
+},
+//It's getting obvious that I need to define a proper error object.
+"sortErrorArray": function sortErrorArray(errorArray){
+    
+    }
+    
+};
