35b90fffe7a431ff1e6c0a9d416d43fb999daf45
Axel Theorin <axel.theorin@gmail.com>
2012-12-05 19:13:43 +0900

    Adjusted util/db-setup.rb for new schema

--- util/db-setup.rb
+++ util/db-setup.rb
@@ -1,5 +1,5 @@
 # make sure we work in the top-level dir
-unless Dir.pwd =~ /master-frontend$/
+unless Dir["*"].include? "util" #Dir.pwd =~ /master-frontend$/
   abort "run from top-level with (j)ruby util/db-setup.rb"
 end
 begin
@@ -14,25 +14,25 @@ end
 begin
   datapath = ARGV[0] ? ARGV[0] : get_datapath
   texts = Dir["#{datapath}/**/*.txt"].collect do |f|
-    File.basename f
+    [f,File.basename(f)]
   end
-  
+  texts = Hash[texts]
   puts "Create tables"
   DataMapper.auto_migrate!
   puts "Create teacher"
   Teacher.create name:"Teacher"
   puts "Create Students"
-  texts.collect {|t| t[4..8]}.uniq.each do |s|
+  texts.values.collect {|t| t[4..8]}.uniq.each do |s|
     Student.create name:"#{s}"
   end
   puts "Create Assignments"
   d = Time.now + (4 * 7 * 24 * 60 * 60)
-  texts.collect {|t| t[10..11]}.uniq.each do |a|
+  texts.values.collect {|t| t[10..11]}.uniq.each do |a|
     Assignment.create title: "#{a}",description: "",deadline: Date.new(d.year, d.month, d.day)
   end
   puts "Create Compositions"
-  texts.each do |t|
-    text = File.open("#{datapath}/#{t}",'r').read
+  texts.each do |path,t|
+    text = File.open(path,'r').read
     student = t[4..8]
     assignment = t[10..11]
     Composition.create content: text, student_id: Student.first(name: student).id, assignment_id: Assignment.first(title: assignment).id
